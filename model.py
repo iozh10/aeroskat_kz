8baf5a12a6cf76b31c776bc680c57a2623af9371
iozh11 <135107119+iozh11@users.noreply.github.com>
2026-07-22 17:32:51 +0300

    2.2

--- model.py
+++ model.py
@@ -4,7 +4,6 @@ import torch
 
 model = YOLO('last.pt')
 
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 if device == 'cuda':
     model.model.half()
@@ -15,17 +14,12 @@ cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 
-frame_count = 0
-log_every_n_frames = 60
-
-print("Запуск детекции. Нажмите 'q' для выхода.")
+print("Нажмите 'q' для выхода.")
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-
-    frame_count += 1
 
     results = model.predict(
         source=frame,
@@ -37,10 +31,6 @@ while cap.isOpened():
     )
 
     annotated_frame = results[0].plot()
-
-    if frame_count % log_every_n_frames == 0:
-        num_objects = len(results[0].boxes)
-        print(f"Кадр {frame_count}: Найдено объектов: {num_objects}")
 
     cv2.imshow("YOLO Detection", annotated_frame)
 
